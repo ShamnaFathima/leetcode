fd70f4dd184de1be090018b42b761ffa4821b917
Shamna Fathima.N <shamnanizam70@gmail.com>
2025-07-31 13:48:14 +0530

    Time: 4 ms (63.69%), Space: 56 MB (82.35%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        double maxSum = 0;
+        for (int i = 0; i < k; i++) {
+            maxSum += nums[i];
+        }
+        double windowSum = maxSum;
+        for (int i = k; i < nums.length; i++) {
+            windowSum += nums[i] - nums[i - k]; 
+            maxSum = Math.max(maxSum, windowSum);
+        }
+
+        return maxSum / k;
+    }
+}
