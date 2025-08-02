361bab9d8445d1092c4c479466bcba7930ecc995
Shamna Fathima.N <shamnanizam70@gmail.com>
2025-08-02 11:33:22 +0530

    Time: 6 ms (90.44%), Space: 55.1 MB (99.59%) - LeetHub

---
+++ 0848-shifting-letters/0848-shifting-letters.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public String shiftingLetters(String s, int[] shifts) {
+        int n = s.length();
+        long totalShift = 0;
+        char[] chars = s.toCharArray();
+
+        for (int i = n - 1; i >= 0; i--) {
+            totalShift = (totalShift + shifts[i]) % 26;
+            chars[i] = (char) ((chars[i] - 'a' + totalShift) % 26 + 'a');
+        }
+
+        return new String(chars);
+    }
+}
